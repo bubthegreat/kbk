46045f626ffcaa91c6af5dc2426494603ef93513
bubthegreat <bubthegreat@gmail.com>
2019-02-16 00:38:21 -0700

    Intermediary commit

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,20 @@
-FROM ubuntu:14.04
+FROM ubuntu:trusty
 
 RUN apt-get -y update
 
-# Install the basics
-RUN apt-get -yqq install make
-RUN apt-get -yqq install gcc-4.8
-RUN apt-get -yqq install libmysqlclient-dev
-RUN apt-get -yqq install csh
-RUN apt-get -yqq install git-all
-RUN apt-get -yqq install libc6-dbg
-RUN apt-get -yqq install gdb
-RUN apt-get -yqq install vim
-RUN apt-get -yqq install mysql-client
+RUN apt-get -y install make
+RUN apt-get -y install gcc
+RUN apt-get -y install libmysqlclient-dev
+RUN apt-get -y install csh
+RUN apt-get -y install git-all
+RUN apt-get -y install libc6-dbg
+RUN apt-get -y install gdb
+RUN apt-get -y install vim
+RUN apt-get -y install mysql-client
 
-# Make gcc-4.8 our default gcc
-RUN ln -s /usr/bin/gcc-4.9 /usr/bin/gcc
+RUN DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
+
+EXPOSE 3306
+
+COPY . /kbk
+RUN cd /kbk/src && make -j8
--- src/mysql.h
+++ src/mysql.h
@@ -1,41 +1,6 @@
-/***************************************************************************
- *  Original Diku Mud copyright (C) 1990, 1991 by Sebastian Hammer,        *
- *  Michael Seifert, Hans Henrik St{rfeldt, Tom Madsen, and Katja Nyboe.   *
- *                                                                         *
- *  Merc Diku Mud improvments copyright (C) 1992, 1993 by Michael          *
- *  Chastain, Michael Quan, and Mitchell Tse.                              *
- *                                                                         *
- *  In order to use any part of this Merc Diku Mud, you must comply with   *
- *  both the original Diku license in 'license.doc' as well the Merc       *
- *  license in 'license.txt'.  In particular, you may not remove either of *
- *  these copyright notices.                                               *
- *                                                                         *
- *  Thanks to abaddon for proof-reading our comm.c and pointing out bugs.  *
- *  Any remaining bugs are, of course, our work, not his.  :)              *
- *                                                                         *
- *  Much time and thought has gone into this software and you are          *
- *  benefitting.  We hope that you share your changes too.  What goes      *
- *  around, comes around.                                                  *
- ***************************************************************************/
-
-/***************************************************************************
-*	ROM 2.4 is copyright 1993-1996 Russ Taylor			   *
-*	ROM has been brought to you by the ROM consortium		   *
-*	    Russ Taylor (rtaylor@pacinfo.com)				   *
-*	    Gabrielle Taylor (gtaylor@pacinfo.com)			   *
-*	    Brian Moore (rom@rom.efn.org)				   *
-*	By using this code, you have agreed to follow the terms of the	   *
-*	ROM license, in the file Tartarus/doc/rom.license                  *
-***************************************************************************/
-
-/***************************************************************************
-*       Tartarus code is copyright (C) 1997-1998 by Daniel Graham          *
-*	In using this code you agree to comply with the Tartarus license   *
-*       found in the file /Tartarus/doc/tartarus.doc                       *
-***************************************************************************/
 MYSQL                   	conn;
 
-#define SQL_SERVER      	"kbk_kbk_sql_1"
+#define SQL_SERVER      	"localhost"
 #define SQL_USER        	"kbkuser"
 #define SQL_PWD         	"kbkpassword"
 #define SQL_DB          	"kbkdatabase"
