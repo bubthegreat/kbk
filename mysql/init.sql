310aa56091c94dd85e64b1416772283f1bf30a0b
Micheal Taylor <bubthegreat@gmail.com>
2026-01-04 00:38:43 -0700

    updating mysql for deprecated method

--- mysql/init.sql
+++ mysql/init.sql
@@ -89,7 +89,6 @@ CREATE TABLE IF NOT EXISTS `charmed` (
   `damroll` int(10) NOT NULL default '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-CREATE USER IF NOT EXISTS `kbkuser`;
-ALTER USER 'kbkuser'@'%' IDENTIFIED WITH mysql_native_password BY 'kbkpassword';
+CREATE USER IF NOT EXISTS 'kbkuser'@'%' IDENTIFIED BY 'kbkpassword';
 GRANT ALL ON `kbkdatabase`.* to 'kbkuser'@'%';
 FLUSH PRIVILEGES;
